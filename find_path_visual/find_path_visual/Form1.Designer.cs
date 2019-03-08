4215994bfb26e7488f4656b8d0d338edcfc5647d
Murat Gunes <48354144+mguneskou@users.noreply.github.com>
2019-03-08 12:40:00 +0000

    Add files via upload

---
+++ find_path_visual/find_path_visual/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace find_path_visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.btn_generate_map = new System.Windows.Forms.Button();
+            this.btn_find_path = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_height = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_width = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lst_path = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.BackColor = System.Drawing.Color.White;
+            this.pb.Location = new System.Drawing.Point(441, 11);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(640, 640);
+            this.pb.TabIndex = 0;
+            this.pb.TabStop = false;
+            // 
+            // btn_generate_map
+            // 
+            this.btn_generate_map.Location = new System.Drawing.Point(6, 72);
+            this.btn_generate_map.Name = "btn_generate_map";
+            this.btn_generate_map.Size = new System.Drawing.Size(149, 46);
+            this.btn_generate_map.TabIndex = 1;
+            this.btn_generate_map.Text = "Generate Map";
+            this.btn_generate_map.UseVisualStyleBackColor = true;
+            this.btn_generate_map.Click += new System.EventHandler(this.btn_generate_map_Click);
+            // 
+            // btn_find_path
+            // 
+            this.btn_find_path.Enabled = false;
+            this.btn_find_path.Location = new System.Drawing.Point(34, 160);
+            this.btn_find_path.Name = "btn_find_path";
+            this.btn_find_path.Size = new System.Drawing.Size(112, 46);
+            this.btn_find_path.TabIndex = 2;
+            this.btn_find_path.Text = "Find Path";
+            this.btn_find_path.UseVisualStyleBackColor = true;
+            this.btn_find_path.Click += new System.EventHandler(this.btn_find_path_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_height);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btn_generate_map);
+            this.groupBox1.Controls.Add(this.txt_width);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(169, 127);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Generate Map";
+            // 
+            // txt_height
+            // 
+            this.txt_height.Enabled = false;
+            this.txt_height.Location = new System.Drawing.Point(55, 46);
+            this.txt_height.Name = "txt_height";
+            this.txt_height.Size = new System.Drawing.Size(100, 20);
+            this.txt_height.TabIndex = 3;
+            this.txt_height.Text = "64";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height";
+            // 
+            // txt_width
+            // 
+            this.txt_width.Location = new System.Drawing.Point(55, 20);
+            this.txt_width.Name = "txt_width";
+            this.txt_width.Size = new System.Drawing.Size(100, 20);
+            this.txt_width.TabIndex = 1;
+            this.txt_width.Text = "64";
+            this.txt_width.TextChanged += new System.EventHandler(this.txt_width_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width";
+            // 
+            // lst_path
+            // 
+            this.lst_path.FormattingEnabled = true;
+            this.lst_path.Location = new System.Drawing.Point(188, 28);
+            this.lst_path.Name = "lst_path";
+            this.lst_path.Size = new System.Drawing.Size(247, 628);
+            this.lst_path.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Path Found";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 663);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lst_path);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_find_path);
+            this.Controls.Add(this.pb);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Find Path";
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.Button btn_generate_map;
+        private System.Windows.Forms.Button btn_find_path;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_height;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_width;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lst_path;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ find_path_visual/find_path_visual/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace find_path_visual
+{
+    public partial class Form1 : Form
+    {
+        int width = 64, height = 64;//initial width, height values
+        Timer t;//renderer
+        Graphics g_character;//will draw the character
+        Bitmap character;//character's bitmap will assigned to image property of the picturebox
+        SolidBrush c_brush = new SolidBrush(Color.Red);//character's colour
+        Point current_location, next_location;//character's location
+        bool game_over = false;//controls the renderer
+        List<Point> path = new List<Point>();//found path
+        int x_factor = 10, y_factor = 10;//aspect ratio
+        
+        public Form1()
+        {
+            InitializeComponent();
+            t = new Timer();
+            t.Interval = 100;//frame rate, 10 fps at the moment
+            t.Tick += render;//event handler for timer's tich event
+        }
+
+        private void render(object sender, EventArgs e)//updates character's position 10 times every second
+        {
+            if(!game_over)//if game_over flag is false
+            {
+                if(current_location.Y >= height * y_factor - y_factor)//character has found the path to bottom
+                {
+                    t.Stop();//path found stop the timer
+                    MessageBox.Show("Path found!");
+                    btn_find_path.Enabled = false;//disable the start button
+                    btn_generate_map.Enabled = true;//enable the map generation button
+                    game_over = true;//set game_over flag to true
+                    lst_path.DataSource = path;//fill listbox with path information
+                }
+                else//character still trying to find the path to bottom
+                {
+                    find_next_move();//gets the next possible position, if there is no any sets the game_over flag as true
+                    current_location = next_location;
+                    path.Add(current_location);//add the found location to the list
+                    g_character.FillRectangle(c_brush, new Rectangle(current_location, new Size(x_factor, y_factor)));//draw the character into next position
+                    pb.Image = character;//set picturebox's image property
+                }
+            }
+            else
+            {
+                t.Stop();
+                MessageBox.Show("No more moves, renew the map!");//there is no move
+                btn_find_path.Enabled = false;//disable the start button
+                btn_generate_map.Enabled = true;//enable the map generation button
+            }
+        }
+
+        void find_next_move()//finds possible next move, tends to move down first, if not possible tries right, then tries left
+        {
+            if(Map_generator.clear_path.Exists(o => o.Y == current_location.Y + y_factor && o.X == current_location.X))//first try to move straight down
+            {
+                next_location = Map_generator.clear_path.Find(o => o.Y == current_location.Y + y_factor && o.X == current_location.X);//next location of character is bottom of the current location
+            }
+            else if(Map_generator.clear_path.Exists(o => o.Y == current_location.Y && o.X == current_location.X + x_factor))//if not try the right one
+            {
+                if(current_location.X < width * x_factor - x_factor)//is character is not on the right border of the picturebox
+                {
+                    next_location = Map_generator.clear_path.Find(o => o.Y == current_location.Y && o.X == current_location.X + x_factor);//next location of character is the right of the current location
+                }
+            }
+            else if (Map_generator.clear_path.Exists(o => o.Y == current_location.Y - y_factor && o.X == current_location.X))//if not try the up one
+            {
+                current_location.Y = current_location.Y - y_factor;//move one pixel back
+                if (Map_generator.clear_path.Exists(o => o.Y == current_location.Y && o.X == current_location.X + x_factor))//check right
+                {
+                    next_location = Map_generator.clear_path.Find(o => o.Y == current_location.Y && o.X == current_location.X + x_factor);//next location of character is right of the current location
+                }
+                else if (Map_generator.clear_path.Exists(o => o.Y == current_location.Y && o.X == current_location.X - x_factor))//check left
+                {
+                    next_location = Map_generator.clear_path.Find(o => o.Y == current_location.Y && o.X == current_location.X - x_factor);//next location of character is left of the current location
+                }
+            }
+            else if(Map_generator.clear_path.Exists(o => o.Y == current_location.Y && o.X == current_location.X - x_factor))//next move is the left
+            {
+                if (current_location.X > 0)//if character is not on the left border of the picturebox
+                {
+                    next_location = Map_generator.clear_path.Find(o => o.Y == current_location.Y && o.X == current_location.X - x_factor);//next location of character is the left of the current location
+                }
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void txt_width_TextChanged(object sender, EventArgs e)
+        {
+            txt_height.Text = txt_width.Text;//for keeping aspect ration constant, txt_height is disabled
+        }
+
+        //starts the game
+        private void btn_find_path_Click(object sender, EventArgs e)
+        {
+            btn_generate_map.Enabled = false;
+            btn_generate_map.Enabled = false;
+            t.Start();
+        }
+
+        //prepares map
+        private void btn_generate_map_Click(object sender, EventArgs e)
+        {
+            if (game_over)//if game is over then reset the variables
+            {
+                //reset all variables
+                game_over = false;//new game starts
+                current_location = new Point(0, 0);//current lcoation is 0, 0
+                next_location = new Point(0, 0);//next lcoation is 0, 0
+                Map_generator.clear_path.Clear();//clear path should be emptied before filling with new locations
+                Map_generator.obstacles.Clear();//obstacles should be emptied before filling with new locations
+                Array.Clear(Map_generator.map, 0, Map_generator.map.Length);//map is being cleared
+                g_character.Clear(Color.Transparent);//character's path is being cleared
+                path.Clear();
+                pb.Image = character;
+                //reset all variables
+            }
+            width = Convert.ToInt32(txt_width.Text);//set width
+            height = Convert.ToInt32(txt_height.Text);//set height
+            x_factor = pb.Width / width;//get the aspect ratio
+            y_factor = pb.Height / height;//get the aspect ratio
+            character = new Bitmap(width * x_factor, height * y_factor);//create the character bitmap
+            g_character = Graphics.FromImage(character);//create the character graphics object
+            g_character.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;//for faster rendering
+            Map_generator.generate_map(width, height, x_factor, y_factor);//generate the map
+            pb.BackgroundImage = Map_generator.bmp;//assign the map to the picturebox as background image
+            btn_generate_map.Enabled = false;//disable map generation button
+            btn_find_path.Enabled = true;//enable path find button
+        }
+    }
+
+    internal static class Map_generator//static class generates map and draws on bitmap
+    {
+        internal static int[,] map;//map variable
+        internal static List<Point> obstacles = new List<Point>();//obstacles list of points
+        internal static List<Point> clear_path = new List<Point>();//clear path list of points
+        static Random rnd = new Random();//random number generator for map creation
+
+        internal static Graphics g;//draws the obstacle rectangles on the bitmap
+        internal static Bitmap bmp;//map bitmap as background image of the picturebox
+        internal static SolidBrush brush;//obstacles are black by default
+
+        /// <summary>
+        /// Generates map with given parameters
+        /// </summary>
+        /// <param name="width">Width of the map in pixels</param>
+        /// <param name="height">Height of the map in pixels</param>
+        /// <param name="x_factor">x aspect ratio</param>
+        /// <param name="y_factor">y aspect ratio</param>
+        internal static void generate_map(int width, int height, int x_factor, int y_factor)
+        {
+            bmp = new Bitmap(width * x_factor, height * y_factor);//map bitmap gets new size
+            g = Graphics.FromImage(bmp);//graphics object is created
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;//for high speed
+            brush = new SolidBrush(Color.Black);//default colour of obstacles is black
+            g.Clear(Color.White);//clears the bitmap to picturebox's backcolor
+            map = new int[width, height];//we store the 0 and 1 information in this variable
+
+            for (int y = 0; y < height; y++)//rows
+            {
+                for (int x = 0; x < width; x++)//columns
+                {
+                    if(y > 1 && y % 5 == 0 || y % 4 == 0)//let obstacles come at every 4th and 5th row
+                    {
+                        map[y, x] = rnd.Next(0, 2);//get next random number 0-1
+                        if (map[y, x] == 1)//if it is 1
+                        {
+                            g.FillRectangle(brush, new Rectangle(new Point(x * x_factor, y * y_factor), new Size(x_factor, y_factor)));//draw the obstacle on bitmap
+                        }
+                    }
+                    //at every iteration of for loops checks the map information and places the obstacles and clear path into related lists
+                    if (map[y, x] == 1)
+                    {
+                        obstacles.Add(new Point(x * x_factor, y * y_factor));
+                    }
+                    else
+                    {
+                        clear_path.Add(new Point(x * x_factor, y * y_factor));
+                    }
+                }
+            }
+        }
+    }
+}
